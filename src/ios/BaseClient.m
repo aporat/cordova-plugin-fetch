7179b389bd5ba9d7535440a87909d1280f228af6
Konstantin Nikiforov <konstantin.nikiforov@cbca.ru>
2020-12-07 15:40:11 +0300

    Update to AFNetworking@77ef5fe
    AFNetworking/AFNetworking@77ef5fed64d98107acd177a90182163a20ba4567

--- src/ios/BaseClient.m
+++ src/ios/BaseClient.m
@@ -70,7 +70,7 @@
   }
   
   __block NSURLSessionDataTask *dataTask = nil;
-  dataTask = [self dataTaskWithRequest:request completionHandler:^(NSURLResponse * __unused response, id responseObject, NSError *error) {
+  dataTask = [self dataTaskWithRequest:request uploadProgress:nil downloadProgress:nil completionHandler:^(NSURLResponse * __unused response, id responseObject, NSError *error) {
     if (error) {
       if (failure) {
         failure(dataTask, error, responseObject);
